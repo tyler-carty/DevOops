5c79895a5f8ccb74e0fdc059fd2ec66fe753cf46
rgeorge8 <rachel.george.uk@outlook.com>
2025-04-24 11:21:46 +0100

    update radar plot to use weekly averages

--- runner.py
+++ runner.py
@@ -5,37 +5,59 @@ import os
 import plotly.express as px
 import plotly.graph_objects as go
 
-
 from ydata_profiling import ProfileReport
 
-data=None
+data = None
 
-for file in (os.listdir(os.getcwd() +'/data')):
+for file in (os.listdir(os.getcwd() + '/data')):
 
-    print(file)
-    if('.csv' in file and 1 == 0):
-        file_content=pd.read_csv(os.getcwd()+ '/data/'+file)
-        file_content['Datetime']= pd.to_datetime(file_content['Date']+'T'+file_content['Time']+'Z')
+
+    if ('.csv' in file and 1 == 0):
+        file_content = pd.read_csv(os.getcwd() + '/data/' + file)
+        file_content['Datetime'] = pd.to_datetime(file_content['Date'] + 'T' + file_content['Time'] + 'Z')
         file_content.set_index('Datetime', inplace=True)
         file_content.drop(['Date', 'Time'], axis=1, inplace=True)
         print(file_content.head())
         # profile= ProfileReport(file_content, tsmode=True, sortby='', title=f'{file} EDA')
         # profile.to_file(f'{file}_EDA.html')
-        fig=px.line_polar(file_content, r=file_content.columns[0], theta=file_content.index, line_close=True)
+        fig = px.line_polar(file_content, r=file_content.columns[0], theta=file_content.index, line_close=True)
         fig.write_html(f'{file}_polar_plot.html')  # Corrected method to save the figure
 
-    if('.csv' in file and 1 == 1):
-        data=pd.read_csv(os.getcwd()+ '/data/'+file)
-        print(data.head())
-        # Select relevant columns
-        columns = ['Step Count (steps)', 'Exercise Minutes (minutes)', 'Mindfulness Minutes (minutes)', 'Deep Sleep (%)', 'REM Sleep (%)', 'Heart Rate (bpm)']
+    if '.csv' in file and 1 == 1:
+        data = pd.read_csv(os.getcwd() + '/data/' + file)
+        # print(data.head())
+        data['Datetime'] = pd.to_datetime(data['Date'] + 'T' + data['Time'] + 'Z')
+        data.set_index('Datetime', inplace=True)
+        data.drop(['Date', 'Time'], axis=1, inplace=True)
+
+        # Select relevant columns (exclude 'Datetime' since it's now the index)
+        columns = ['Step Count (steps)', 'Exercise Minutes (minutes)', 'Mindfulness Minutes (minutes)',
+                   'Deep Sleep (%)', 'REM Sleep (%)', 'Heart Rate (bpm)']
         data = data[columns]
 
-        # Normalize the data
-        normalized_data = (data - data.min()) / (data.max() - data.min())
+        # Group data by week and calculate weekly averages
+        weekly_data = data.groupby(pd.Grouper(freq='W')).sum()
+        # weekly_data = data.groupby(pd.Grouper(freq='W')).mean()
+
+        # Normalize the weekly data based on healthy reference values
+        healthy_reference = {
+            'Step Count (steps)': 70000,  # Average weekly steps
+            'Exercise Minutes (minutes)': 210,  # Recommended weekly exercise
+            'Mindfulness Minutes (minutes)': 70,  # Suggested mindfulness duration
+            'Deep Sleep (%)': 20,  # Percentage of total sleep
+            'REM Sleep (%)': 25,  # Percentage of total sleep
+            'Heart Rate (bpm)': 70  # Average resting heart rate
+        }
+
+        normalized_weekly_data = weekly_data.copy()
+        print('here')
+        print(normalized_weekly_data.head())
+
+        for column in columns:
+            normalized_weekly_data[column] = weekly_data[column] / healthy_reference[column]
 
         # Average values for radar graph
-        averages = normalized_data.mean()
+        averages = normalized_weekly_data.mean()
 
         # Create radar graph
         fig = go.Figure()
@@ -52,4 +74,3 @@ for file in (os.listdir(os.getcwd() +'/data')):
         )
 
         fig.write_html(f'{file}_radar_plot.html')
-
