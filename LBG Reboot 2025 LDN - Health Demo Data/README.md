0b14c885ac9dc7622e180fb311f9bcf1fbf5463f
rgeorge8 <rachel.george.uk@outlook.com>
2025-04-24 08:54:14 +0100

    add profile report to view files

--- LBG Reboot 2025 LDN - Health Demo Data/README.md
+++ LBG Reboot 2025 LDN - Health Demo Data/README.md
@@ -1,1 +1,2 @@
-test
+create charts for each persona based on heart rate, etc
+mean/sd etc
--- runner.py
+++ runner.py
@@ -2,10 +2,16 @@ import pandas as pd
 import numpy as np
 import os
 
+from ydata_profiling import ProfileReport
+
+data=None
+
 for file in (os.listdir(os.getcwd() +'/data')):
+
     print(file)
     if('.csv' in file):
-        file_content=open('./data/'+file, 'r+')
+        file_content=pd.read_csv(file)
 
-        print(file_content.read())
-
+        print(file_content.head())
+        profile= ProfileReport(file_content, tsmode=True, sortby='', title=f'{file} EDA')
+        profile.to_file(f'{file}_EDA.html')
