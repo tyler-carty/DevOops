bd6b1d479d723ef006ab980ef4189ba6da97cb92
rgeorge8 <rachel.george.uk@outlook.com>
2025-04-24 09:16:27 +0100

    add data analysis library and update file reading to match requirement

--- runner.py
+++ runner.py
@@ -10,8 +10,10 @@ for file in (os.listdir(os.getcwd() +'/data')):
 
     print(file)
     if('.csv' in file):
-        file_content=pd.read_csv(file)
-
+        file_content=pd.read_csv(os.getcwd()+ '/data/'+file)
+        file_content['Datetime']= pd.to_datetime(file_content['Date']+'T'+file_content['Time']+'Z')
+        file_content.set_index('Datetime', inplace=True)
+        file_content.drop(['Date', 'Time'], axis=1, inplace=True)
         print(file_content.head())
         profile= ProfileReport(file_content, tsmode=True, sortby='', title=f'{file} EDA')
         profile.to_file(f'{file}_EDA.html')
